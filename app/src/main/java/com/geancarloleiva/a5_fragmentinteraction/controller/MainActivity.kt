b70f41c158699cb65955926ee3874b900c74017e
geanclv <geanclv@gmail.com>
2022-05-31 11:35:00 -0500

    Initial commit

---
+++ app/src/main/java/com/geancarloleiva/a5_fragmentinteraction/controller/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.geancarloleiva.a5_fragmentinteraction.controller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.geancarloleiva.a5_fragmentinteraction.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
