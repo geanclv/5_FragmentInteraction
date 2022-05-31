893d5d5f4aee1f5d354a280da0a964c74d5b471e
geanclv <geanclv@gmail.com>
2022-05-31 13:07:15 -0500

    Creando fragmentos e introduciéndolos en el Main Activity. Además, realizando operaciones en cada fragmento.

--- app/src/main/java/com/geancarloleiva/a5_fragmentinteraction/ui/SecondFragment.kt
+++ app/src/main/java/com/geancarloleiva/a5_fragmentinteraction/ui/SecondFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.geancarloleiva.a5_fragmentinteraction.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -16,20 +17,18 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [FirstFragment.newInstance] factory method to
+ * Use the [SecondFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class FirstFragment : Fragment() {
+class SecondFragment : Fragment() {
 
     lateinit var fragmentView: View
-
     // TODO: Rename and change types of parameters
     /*private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
@@ -40,16 +39,17 @@ class FirstFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        fragmentView = inflater.inflate(R.layout.fragment_first, container, false)
+        // Inflate the layout for this fragment
+        fragmentView = inflater.inflate(R.layout.fragment_second, container, false)
 
-        val lblFirstFragment: TextView = fragmentView.findViewById(R.id.lblFirstFragment)
-        val btnFirstFragment: Button = fragmentView.findViewById(R.id.btnFirstFragment)
+        val lblSecondFragment: TextView = fragmentView.findViewById(R.id.lblSecondFragment)
+        val btnSecondFragment: Button = fragmentView.findViewById(R.id.btnSecondFragment)
 
-        btnFirstFragment.setOnClickListener {
-            lblFirstFragment.text = "Hello again"
+        btnSecondFragment.setOnClickListener{
+            lblSecondFragment.text = "2do click"
+            Toast.makeText(context, "Hello there", Toast.LENGTH_SHORT).show()
         }
 
-        // Inflate the layout for this fragment
         return fragmentView
     }
 
@@ -60,12 +60,12 @@ class FirstFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment FirstFragment.
+         * @return A new instance of fragment SecondFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            FirstFragment().apply {
+            SecondFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
